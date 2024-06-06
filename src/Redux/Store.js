688735fd5765664de852e88b610af21568a0600f
inductionotg <ritesh2000.sinha@gmail.com>
2024-06-06 12:10:44 +0530

    Added Reducers

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,7 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
-
+import authSliceReducer from '../Redux/Slice/AuthSlice'
 const store = configureStore({
-    reducer:{},
+    reducer:{
+        auth:authSliceReducer
+    },
     devTools:true
 })
 export default store
