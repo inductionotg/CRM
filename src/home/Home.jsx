20046b7ff592199ca654bd3567c7db4b38eef825
inductionotg <ritesh2000.sinha@gmail.com>
2024-07-15 08:13:49 +0530

    Added Pie charts

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,7 @@
 
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
+import { Pie } from "react-chartjs-2";
 import { GiPencil } from "react-icons/gi";
 import { ImCross } from "react-icons/im";
 import { TbProgress } from "react-icons/tb";
@@ -8,10 +11,22 @@ import { TiTickOutline } from "react-icons/ti";
 import Card from '../components/Card'
 import useTickets from "../hooks/useTickets";
 import HomeLayout from "../layout/HomeLayout"
-
+Chart.register(CategoryScale);
 function Home(){
     const ticketState = useTickets()
     console.log("cutsdom",ticketState)
+
+    const pieCharData = {
+        labels:Object.keys(ticketState.ticketDistribution),
+        fontColor:"white",
+        datasets:[
+            {
+                data:Object.values(ticketState.ticketDistribution),
+                backgroundColor:["yellow","red","green","blue","white"],
+                borderColor:["yellow","red","green","blue","white"]
+            }
+        ]
+    }
     return (
         <HomeLayout>
            {ticketState.ticketDistribution && <div className="flex gap-5 justify-center items-center mt-10 flex-wrap">
@@ -51,7 +66,11 @@ function Home(){
                     icon={ <ImCross size={28} />}
                 />
                    
-                
+                <div className="mt-10 flex justify-center items-center">
+                    <div className="w-96 h-96">
+                        <Pie data={pieCharData} />
+                    </div>
+                </div>
     
             </div>
             
